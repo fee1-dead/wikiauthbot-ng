2c888bfeb4c77d0fc80bce7b052b1fdbb4119d34
Deadbeef <ent3rm4n@gmail.com>
2023-12-17 22:43:56 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = with pkgs; pkgs.mkShell {
+        buildInputs = [ gcc pkg-config ];
+      };
+    });
+}
