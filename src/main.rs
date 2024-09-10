9e2f5d2e00b6246370127272288f69eb42a3a9d0
Deadbeef <ent3rm4n@gmail.com>
2024-09-10 17:28:55 +0800

    extra comma

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
             tracing::error!("Failed to start bot: {:?}", error)
         }
         poise::FrameworkError::Command { error, ctx, .. } => {
-            tracing::error!("Error in command `{}`: {:?}", ctx.command().name, error,);
+            tracing::error!("Error in command `{}`: {:?}", ctx.command().name, error);
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
